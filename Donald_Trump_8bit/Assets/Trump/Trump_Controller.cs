4f132320f35faffc628422189ed5f6208c5a3da3
matt9234 <kratoes123@live.com>
2018-09-18 17:19:06 -0600

    -Added very basic motion controls for Trump      -they need tweaking for correct feeling speed      -need to have animation interrupt for walking

--- Donald_Trump_8bit/Assets/Trump/Trump_Controller.cs
+++ Donald_Trump_8bit/Assets/Trump/Trump_Controller.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Trump_Controller : MonoBehaviour {
+    //variable for controllability
+    // using for cutscenes and whatnot
+    bool isControllable;
 
+    //Trump stuff
+    public GameObject Trump;
+    private Animator trumpAnim;
+    private SpriteRenderer trumpSR;
+
+  
 	// Use this for initialization
 	void Start () {
-		
+        isControllable = true;
+        trumpAnim = Trump.GetComponent<Animator>();
+        trumpSR = Trump.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        //movement handling
+        if (isControllable)
+        {
+            if(Input.GetAxis("Horizontal") < 0f)
+            {
+                trumpAnim.SetBool("Run", true);
+                trumpSR.flipX = true;
+                Trump.transform.Translate(Time.deltaTime * -10.0f, 0.0f, 0.0f);
+            }
+            else if (Input.GetAxis("Horizontal") > 0f)
+            {
+                trumpAnim.SetBool("Run", true);
+                trumpSR.flipX = false;
+                Trump.transform.Translate(Time.deltaTime * 10.0f, 0.0f, 0.0f);
+            }
+            else
+            {
+                trumpAnim.SetBool("Run", false);
+            }
+
+        }
 	}
 }
