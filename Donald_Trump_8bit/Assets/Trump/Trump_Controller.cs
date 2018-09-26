a9a272716dffd8a940a7a8427fe010ca40bb31d2
matt9234 <kratoes123@live.com>
2018-09-26 17:11:10 -0600

    -Slowed Trump's speed -Added space for jumping, need to add a 2d rigidbody onto trump and and platform for him to stand on

--- Donald_Trump_8bit/Assets/Trump/Trump_Controller.cs
+++ Donald_Trump_8bit/Assets/Trump/Trump_Controller.cs
@@ -2,47 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trump_Controller : MonoBehaviour {
+public class Trump_Controller : MonoBehaviour
+{
     //variable for controllability
     // using for cutscenes and whatnot
     bool isControllable;
+    bool hasJump;
+    bool hasDoubleJump;
 
     //Trump stuff
     public GameObject Trump;
     private Animator trumpAnim;
     private SpriteRenderer trumpSR;
 
-  
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         isControllable = true;
+        hasJump = true;
+        hasDoubleJump = true;
         trumpAnim = Trump.GetComponent<Animator>();
         trumpSR = Trump.GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         //movement handling
         if (isControllable)
         {
-            if(Input.GetAxis("Horizontal") < 0f)
+            if (Input.GetAxis("Horizontal") < 0f)
             {
                 trumpAnim.SetBool("Run", true);
                 trumpSR.flipX = true;
-                Trump.transform.Translate(Time.deltaTime * -10.0f, 0.0f, 0.0f);
+                Trump.transform.Translate(Time.deltaTime * -5.0f, 0.0f, 0.0f);
             }
             else if (Input.GetAxis("Horizontal") > 0f)
             {
                 trumpAnim.SetBool("Run", true);
                 trumpSR.flipX = false;
-                Trump.transform.Translate(Time.deltaTime * 10.0f, 0.0f, 0.0f);
+                Trump.transform.Translate(Time.deltaTime * 5.0f, 0.0f, 0.0f);
             }
             else
             {
                 trumpAnim.SetBool("Run", false);
             }
 
+            //jumping handling
+
+            if (Input.GetAxis("Jump") > 0.0f)
+            {
+                if (hasJump)
+                {
+                    trumpAnim.SetTrigger("Jump");
+                    Trump.transform.Translate(Time.deltaTime * 0.0f, 1.0f, 0.0f);           
+                    hasJump = false;
+                }
+                else if (hasDoubleJump)
+                {
+                    trumpAnim.SetTrigger("Jump");
+                    Trump.transform.Translate(Time.deltaTime * 0.0f, 1.0f, 0.0f);           
+                    hasDoubleJump = false;
+                }
+
+            }
+
         }
-	}
+    }
 }
